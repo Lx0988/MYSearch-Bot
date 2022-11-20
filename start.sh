68edc5811229a9c7ab5a8f44c501aaf6e8ac93b0
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-21 00:31:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/0AIB/DFF-Media-Search.git /EvaMaria
+  git clone https://github.com/0AIB/DFF-Media-Search.git /LxInline
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /LxInline
 fi
-cd /EvaMaria
+cd /LxInline
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m bot
