48b8cb268d8cf6d0949469ef4ad84821206a0ce4
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-20 23:45:34 +0530

    Update genlink.py

--- bot/plugins/genlink.py
+++ bot/plugins/genlink.py
@@ -1,9 +1,9 @@
 import re
 from pyrogram import filters, Client, enums
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
-from info import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE
-from database.ia_filterdb import unpack_new_file_id
-from utils import temp
+from bot import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE
+from bot.database.ia_filterdb import unpack_new_file_id
+from bot.database import temp
 import re
 import os
 import json
