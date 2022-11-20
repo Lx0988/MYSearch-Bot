2ca8db374588314a01867453156b542e7a8ad798
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-21 04:08:19 +0530

    Update __init__.py

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,5 +1,5 @@
-from pyrogram import filters
+# from pyrogram import filters
 from bot.database.lx.utils import temp, get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, \
     broadcast_messages, extract_user, get_file_id, last_online, parser, split_quotes
 
-media_filter = filters.document | filters.video | filters.audio
+# media_filter = filters.document | filters.video | filters.audio
