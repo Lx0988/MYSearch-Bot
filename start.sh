1f443abf2649a50e71ddfd76a9a7724c4f20081c
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-21 00:32:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/0AIB/DFF-Media-Search.git /LxInline
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LxInline
-fi
-cd /LxInline
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m bot
