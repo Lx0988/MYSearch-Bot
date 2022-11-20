59fbb4d39f9629955dc6f8db21c2f02a09c99971
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-21 00:09:31 +0530

    Update __init__.py

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,1 +1,2 @@
-from bot.database.lx.utils import temp, get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
+from bot.database.lx.utils import temp, get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, /
+    broadcast_messages
