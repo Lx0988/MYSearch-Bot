2b50b85ebbcd97a2eb939793aec306c96153c24b
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-20 23:25:48 +0530

    Update broadcast.py

--- bot/plugins/broadcast.py
+++ bot/plugins/broadcast.py
@@ -2,9 +2,9 @@
 from pyrogram import Client, filters
 import datetime
 import time
-from database.users_chats_db import db
-from info import ADMINS
-from utils import broadcast_messages
+from bot.database.users_chats_db import db
+from bot import ADMINS
+from bot.database.lx.utils import broadcast_messages
 import asyncio
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
