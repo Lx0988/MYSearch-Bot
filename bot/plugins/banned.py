b8c37d8b82b9797fbee9f5b4ed8dad6c8e3ba342
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-21 03:46:51 +0530

    Update banned.py

--- bot/plugins/banned.py
+++ bot/plugins/banned.py
@@ -1,9 +1,15 @@
+import asyncio
+import datetime
+import time
 from pyrogram import Client, filters
-from bot.database import temp
+from bot.database import temp, broadcast_messages
 from pyrogram.types import Message
 from bot.database.users_chats_db import db
+from bot.database.ia_filterdb import save_file
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from bot import SUPPORT_CHAT
+from bot import SUPPORT_CHAT, ADMINS, CHANNELS
+
+media_filter = filters.document | filters.video | filters.audio
 
 async def banned_users(_, client, message: Message):
     return (
@@ -38,3 +44,53 @@ async def grp_bd(bot, message):
     except:
         pass
     await bot.leave_chat(message.chat.id)
+
+        
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+# https://t.me/GetTGLink/4178
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting your messages...'
+    )
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    blocked = 0
+    deleted = 0
+    failed =0
+
+    success = 0
+    async for user in users:
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if pti:
+            success += 1
+        elif pti == False:
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
+                failed += 1
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+
+
+@Client.on_message(filters.chat(CHANNELS) & media_filter)
+async def media(bot, message):
+    """Media Handler"""
+    for file_type in ("document", "video", "audio"):
+        media = getattr(message, file_type, None)
+        if media is not None:
+            break
+    else:
+        return
+
+    media.file_type = file_type
+    media.caption = message.caption
+    await save_file(media)
