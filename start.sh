c8b74ad8573e7e4120cae68050a03ca6a00be9d5
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-21 00:48:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/0AIB/LxInline.git /LxInline
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /LxInline
+fi
+cd /LxInline
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m bot
