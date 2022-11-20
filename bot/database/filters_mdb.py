b758f2a1747394bb15a69138fd5a5e37136b0587
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-20 23:57:26 +0530

    Update filters_mdb.py

--- bot/database/filters_mdb.py
+++ bot/database/filters_mdb.py
@@ -1,6 +1,6 @@
 import pymongo
 from pyrogram import enums
-from info import DATABASE_URI, DATABASE_NAME
+from bot import DATABASE_URI, DATABASE_NAME
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
