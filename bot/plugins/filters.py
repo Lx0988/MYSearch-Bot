88750ab1bc55dfb074ed80ee150b1b68896d6039
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-20 23:31:21 +0530

    Update filters.py

--- bot/plugins/filters.py
+++ bot/plugins/filters.py
@@ -1,16 +1,16 @@
 import io
 from pyrogram import filters, Client, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from database.filters_mdb import(
+from bot.database.filters_mdb import(
    add_filter,
    get_filters,
    delete_filter,
    count_filters
 )
 
-from database.connections_mdb import active_connection
-from utils import get_file_id, parser, split_quotes
-from info import ADMINS
+from bot.database.connections_mdb import active_connection
+from bot.database.lx.utils import get_file_id, parser, split_quotes
+from bot import ADMINS
 
 
 @Client.on_message(filters.command(['filter', 'add']) & filters.incoming)
