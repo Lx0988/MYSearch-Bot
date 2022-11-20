4db9c261f1e21d71d895ea5f575808f6f1298741
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-20 23:41:11 +0530

    Update p_ttishow.py

--- bot/plugins/p_ttishow.py
+++ bot/plugins/p_ttishow.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
-from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS
-from database.users_chats_db import db
-from database.ia_filterdb import Media
-from utils import get_size, temp, get_settings
-from Script import script
+from bot import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS
+from bot.database.users_chats_db import db
+from bot.database.ia_filterdb import Media
+from bot.database import get_size, temp, get_settings
+from translation import script
 from pyrogram.errors import ChatAdminRequired
 
 """-----------------------------------------https://t.me/GetTGLink/4179 --------------------------------------"""
