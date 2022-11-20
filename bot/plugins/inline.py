bc24385052cdb20b2129ff8bb82fdc2aed243784
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-20 23:43:19 +0530

    Update inline.py

--- bot/plugins/inline.py
+++ bot/plugins/inline.py
@@ -2,9 +2,9 @@ import logging
 from pyrogram import Client, emoji, filters
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
-from database.ia_filterdb import get_search_results
-from utils import is_subscribed, get_size, temp
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from bot.database.ia_filterdb import get_search_results
+from bot.database import is_subscribed, get_size, temp
+from bot import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
