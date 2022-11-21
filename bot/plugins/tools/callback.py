85922b20f689f259170ce8a310063ea11585746c
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-21 12:14:24 +0530

    Update callback.py

--- bot/plugins/tools/callback.py
+++ bot/plugins/tools/callback.py
@@ -37,7 +37,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.ABOUT_TXT.format(temp.U_NAME),
+            text=script.ABOUT_TXT.format(temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )   
