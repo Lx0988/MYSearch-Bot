333acd0fc781832cf71117692eb559104e2dd99a
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-21 12:37:55 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,8 +1,13 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
 
-    ABOUT_TXT = """• Bot Name: Media Search Bot
+    START_TXT = """**Hi, I'm Media Search bot**
+• Join update channel to use this bot. @DFF_UPDATES
+
+• Here you can search files in inline mode. Just press follwing buttons and start searching.
+"""
+
+
+    ABOUT_TXT = """• Bot Name: <a href=https://t.me/{}>Media Search Bot</a>
 • Creator: <a href="https://t.me/CvSubhan">Lx</a>
 • Language: Python3
 • Library: Pyrogram 
@@ -11,29 +16,8 @@ class script(object):
 • Build Status: v1.0.1 
 • Database: MongoDB
 • Max capacity: <a href="https://t.me/DFF_UPDATE">Don't click here</a>"""
+    
 
-    ADMIN_TXT = """Help: <b>Admin mods</b>
-
-<b>NOTE:</b>
-This module only works for my admins
-
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
 
     LOG_TEXT_P = """#NewUser
 ID - <code>{}</code>
